7d777ce7f635ac5efc7d9729e6f9cb4e376f2e8a
Jeremy Winnett <winnettjeremy@gmail.com>
2025-08-30 20:35:09 -0400

    moved methods out of game class to run better refactored main game loop to add play again   prompt

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,19 +2,13 @@ require_relative 'human'
 require_relative 'game'
 require_relative 'items'
 
-
-
 class Comp
-  attr_reader :comp_guess
   def initialize
-    @comp_guess = ["purple", "blue", "green", "pink", "black", "grey"].shuffle.sample(4)
-  end
-  def comp_guess
-    #need to put red where wrong color and white where a correct one is
-    @comp_guess
+    @comp_guess = %w[purple blue green pink black grey].shuffle.sample(4)
   end
 
+  attr_reader :comp_guess
 end
 
-#comp = Comp.new
-#puts comp.comp_guess
+# comp = Comp.new
+# puts comp.comp_guess
